0a26e212bbe1d8cbea55a635cf31d95d05defa15
Louiza Mak <louiza.mak@gmail.com>
2024-03-14 10:44:37 -0500

    abstract ImageCard tags and add like button

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,14 +1,27 @@
 import "./imagecard.css";
+import { useState } from "react";
 
 function ImageCard({ post }) {
+    const [isLiked, setIsLiked] = useState(false);
+
+    function handleClick(event) {
+        setIsLiked(isLiked => !isLiked);
+        if(isLiked) {
+            post.likes = post.likes - 1;
+        } else {
+            post.likes = post.likes + 1;
+        }
+    }
+
     return (
         <div className="card">
             <img src={post.image} alt=""/>
             <div className="card-content">
                 <h2>{post.user}</h2>
                 <p>{post.caption}</p>
-                <p>{post.tags}</p>
+                <p>{post.tags.map(tag => `#${tag}`)}</p>
                 <p>{post.likes} likes</p>
+                <i className="icon-heart-empty" onClick={handleClick}>{isLiked ? '♥' : '♡' }</i> 
             </div>
         </div>
     )
