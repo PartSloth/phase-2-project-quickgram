808c84f00c3448fc1c10bec3d5eb09e63a816e09
Louiza Mak <louiza.mak@gmail.com>
2024-03-14 16:12:06 -0500

    add additional documents to json-server database and refresh function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,28 @@
 import { useOutletContext } from "react-router-dom";
+import { useEffect, useState } from "react";
 import ImageCard from "./ImageCard";
 import "./imagecard.css";
 
 function Home() {
     const posts = useOutletContext();
+    const [postsDisplayed, setPostsDisplayed] = useState([])
+    const shuffle = posts.sort(() => 0.5 - Math.random()).slice(0,20)
+
+    useEffect(() => {
+        setPostsDisplayed(shuffle)
+    }, [posts])
+
+    function handleRefresh(event) {
+        setPostsDisplayed(shuffle);
+    }
 
     return(
         <div>
             <h1>Home</h1>
             <div className="card-container">
-                {posts.map(post => <ImageCard key={post.id} post={post}/>)}
+                {postsDisplayed.map(post => <ImageCard key={post.id} post={post}/>)}
             </div>
+            <p onClick={handleRefresh}>Refresh</p>
         </div>
     )
 }
